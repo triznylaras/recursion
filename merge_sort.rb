d0d89943868bc48ded7c3670d4b565af713377a7
triznylaras <triznylaras@gmail.com>
2021-12-14 17:49:15 +0700

    create recursive merge_sort method

--- merge_sort.rb
+++ merge_sort.rb
@@ -13,3 +13,40 @@ puts "----iterative fibonacci----"
 p fibs(9)
 puts "----recursive fibonacci----"
 p fibs_rec(9)
+
+def merge_sort(array)
+  return array if array.size < 2
+  left = merge_sort(array[0...array.size / 2])
+  right = merge_sort(array[array.size / 2...array.size])
+  sort(left, right)
+end
+
+def sort(left, right, array = [])
+  (left.size + right.size).times do
+    if left.empty?
+      array << right.shift
+    elsif right.empty?
+      array << left.shift
+    else
+      comparison = left <=> right
+      if comparison == -1
+        array << left.shift
+      elsif comparison == 1
+        array << right.shift
+      else
+        array << left.shift
+      end
+    end
+  end
+  array
+end
+
+array = []
+10.times do
+  array << rand(10)
+end
+puts "----recursive merge sort----"
+puts "given array : "
+p array
+puts "sorted array : "
+p merge_sort(array)
