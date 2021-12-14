d37505c02200d1538803f897ae2286c8772002d4
triznylaras <triznylaras@gmail.com>
2021-12-14 16:37:17 +0700

    add method fibs_rec to solve fib recursively

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,9 +1,15 @@
 def fibs(n)
   array = [0, 1]
-  return n if n < 2
-  (n-1).times { |i| array.push(array[-1] + array[-2]) }
+  n < 2 ? n : (n - 1).times { array.push(array[-1] + array[-2]) }
   array
 end
 
+def fibs_rec(n)
+  n < 2 ? n : fibs_rec(n - 1) + fibs_rec(n - 2)
+end
+
 fib_seq = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+puts "----iterative fibonacci----"
 p fibs(9)
+puts "----recursive fibonacci----"
+p fibs_rec(9)
