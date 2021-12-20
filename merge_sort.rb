d3d604b315fdf1a66edceeb7088f0da2262ee96e
triznylaras <triznylaras@gmail.com>
2021-12-20 17:42:13 +0700

    refactor code

--- merge_sort.rb
+++ merge_sort.rb
@@ -8,14 +8,14 @@ def fibs_rec(n)
   n < 2 ? n : fibs_rec(n - 1) + fibs_rec(n - 2)
 end
 
-fib_seq = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-puts "----iterative fibonacci----"
+puts '----iterative fibonacci----'
 p fibs(9)
-puts "----recursive fibonacci----"
+puts '----recursive fibonacci----'
 p fibs_rec(9)
 
 def merge_sort(array)
   return array if array.size < 2
+
   left = merge_sort(array[0...array.size / 2])
   right = merge_sort(array[array.size / 2...array.size])
   sort(left, right)
@@ -28,25 +28,30 @@ def sort(left, right, array = [])
     elsif right.empty?
       array << left.shift
     else
-      comparison = left <=> right
-      if comparison == -1
-        array << left.shift
-      elsif comparison == 1
-        array << right.shift
-      else
-        array << left.shift
-      end
+      compare(left, right, array)
     end
   end
   array
+end
+
+def compare(left, right, array)
+  comparison = left <=> right
+  array << case comparison
+           when -1
+             left.shift
+           when 1
+             right.shift
+           else
+             left.shift
+           end
 end
 
 array = []
 10.times do
   array << rand(10)
 end
-puts "----recursive merge sort----"
-puts "given array : "
+puts '----recursive merge sort----'
+puts 'given array : '
 p array
-puts "sorted array : "
+puts 'sorted array : '
 p merge_sort(array)
